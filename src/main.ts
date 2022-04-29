a0ef7a6492d53d9c24253d8100af9a1577226074
cristsr <styven21121@gmail.com>
2022-04-28 19:59:30 -0500

    scaffolding

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import { ENV } from 'environment';
+import { listRoutes } from 'utils';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning();
+
+  const showDocs: boolean = configService.get(ENV.SHOW_DOCS);
+
+  if (showDocs) {
+    const config = new DocumentBuilder()
+      .setTitle('Meet app')
+      .setDescription('Meet app API description')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs', app, document, { useGlobalPrefix: false });
+  }
+
+  const port = configService.get(ENV.PORT);
+
+  await app.listen(port);
+
+  listRoutes(app);
+
+  Logger.log(`App running at port ${port}`, 'Bootstrap');
+}
+bootstrap();
