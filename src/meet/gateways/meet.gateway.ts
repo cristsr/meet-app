ed9deb2fca1ac7601b26dab9566c737278a96eec
cristsr <styven21121@gmail.com>
2022-05-02 21:52:54 -0500

    refactor: replace object to string

--- src/meet/gateways/meet.gateway.ts
+++ src/meet/gateways/meet.gateway.ts
@@ -63,7 +63,7 @@ export class MeetGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   @SubscribeMessage('leave')
-  onLeave(socket: Socket, { room }): void {
+  onLeave(socket: Socket, room): void {
     this.logger.log(
       `Client ${socket.id} - ${socket.data.name} left room ${room}`,
     );
